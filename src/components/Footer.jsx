c117a214969d409a6cacb1e10e89fb5695a58037
zahidHasanExt <zahid.hasan.ext@holcim.com>
2026-06-24 16:55:25 +0600

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,49 @@
-import React from 'react';
-import './Footer.css';
+import React, { useState } from "react";
+import "./Footer.css";
+import { Link } from "react-router";
 
 function Footer() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <footer className="footer">
       <div className="footer-logo">
-        <a href="/">Zahid<span> Hasan</span></a>
+        <a href="/">
+          Zahid<span> Hasan</span>
+        </a>
       </div>
-      
-      <nav className="footer-nav">
-        <a href="#home">HOME</a>
-        <a href="#about">ABOUT</a>
-        <a href="#works">WORKS</a>
-        <a href="#contact">CONTACT</a>
+
+      <nav className={`nav-links ${isMenuOpen ? "mobile-open" : ""}`}>
+        <Link
+          to="/"
+          className={location.pathname === "/" ? "active" : ""}
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className={location.pathname === "/about" ? "active" : ""}
+          onClick={() => setIsMenuOpen(false)}
+        >
+          About
+        </Link>
+        <Link
+          to="/works"
+          className={location.pathname === "/works" ? "active" : ""}
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Works
+        </Link>
+        <Link
+          to="/contact"
+          className={location.pathname === "/contact" ? "active" : ""}
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Contact
+        </Link>
       </nav>
-      
-      <div className="copyright">
+
+      <div className="copyright" style={{ marginTop: "40px" }}>
         &copy; All rights reserved by <span>Zahid Hasan</span>
       </div>
     </footer>
